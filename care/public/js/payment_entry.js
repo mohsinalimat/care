362e62ae463a91e9945412e6b31147666ffb56fa
smehta <bhagwatmehta779@gmail.com>
2021-12-06 11:38:34 +0530

    updated payment get outstanding

--- care/public/js/payment_entry.js
+++ care/public/js/payment_entry.js
@@ -1,6 +1,18 @@
 frappe.ui.form.on('Payment Entry', {
     validate: function(frm){
+        frm.trigger('set_cost_center')
+    },
+    party: function(frm){
+        var filters = {}
+        frappe.run_serially([
+            () => frm.events.get_outstanding_documents(frm, filters),
+            () => frm.trigger('set_cost_center')
+        ])
+    },
+    set_cost_center: function(frm){
         if(frm.doc.references){
+            let grand_total = 0
+            let out_total = 0
             for (var row of frm.doc.references) {
                 if (row.reference_doctype == "Purchase Invoice" || row.reference_doctype == "Sales Invoice") {
                     if (row.reference_doctype == "Purchase Invoice") {
@@ -11,14 +23,14 @@ frappe.ui.form.on('Payment Entry', {
                         var doctype = "Sales Invoice";
                         var fieldname = "cost_center";
                     }
+                    grand_total += row.total_amount
+                    out_total += row.outstanding_amount
                     get_sales_order(row, doctype, fieldname);
                 }
             }
+            frm.set_value('grand_total', grand_total);
+            frm.set_value('total_outstanding', out_total);
         }
-    },
-    party: function(frm){
-        var filters = {}
-        frm.events.get_outstanding_documents(frm, filters);
     }
 });
 
