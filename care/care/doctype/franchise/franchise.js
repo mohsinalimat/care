e9f86fd8e8ea56c90eb32502e5ecb7af611ec65f
smehata <bhagwatmehta779@gmail.com>
2022-06-08 14:41:44 +0530

    franchies doctype added

---
+++ care/care/doctype/franchise/franchise.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2022, RF and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Franchise', {
+    setup: function(frm) {
+        frm.fields_dict['franchise_list'].grid.get_field("warehouse").get_query = function(doc, cdt, cdn) {
+            return {
+                filters: {is_group: 0}
+            }
+        }
+    }
+});
---
+++ care/care/doctype/franchise/test_franchise.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, RF and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFranchise(unittest.TestCase):
+	pass
