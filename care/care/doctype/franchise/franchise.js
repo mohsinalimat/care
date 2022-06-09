1d995ac856c1d791b773f394df83d69cdd93454a
smehata <bhagwatmehta779@gmail.com>
2022-06-09 10:56:01 +0530

    updated

--- care/care/doctype/franchise/franchise.js
+++ care/care/doctype/franchise/franchise.js
@@ -5,7 +5,10 @@ frappe.ui.form.on('Franchise', {
     setup: function(frm) {
         frm.fields_dict['franchise_list'].grid.get_field("warehouse").get_query = function(doc, cdt, cdn) {
             return {
-                filters: {is_group: 0}
+                filters: {
+                    is_group: 0,
+                    is_franchise: 1
+                }
             }
         }
     }
