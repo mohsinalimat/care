68b772d811c2ad5091682c48cda60ebfa407f38a
hamzaali15 <hamzaali15>
2022-06-16 22:04:57 +0500

    Brand Wise Set Report Updated

--- care/care/report/brand_wise_set/brand_wise_set.js
+++ care/care/report/brand_wise_set/brand_wise_set.js
@@ -17,5 +17,20 @@ frappe.query_reports["Brand Wise Set"] = {
 			"fieldtype": "Select",
 			"options": ["", __("Draft"), __("To Bill"), __("Completed"), __("Return Issued"), __("Cancelled"), __("Closed")]
 		},
+		{
+			"fieldname": "order_receiving",
+			"fieldtype": "Link",
+			"label": "Order Receiving",
+			"mandatory": 0,
+			"options": "Order Receiving"
+		},
+		{
+			"fieldname":"supplier",
+			"label": __("Supplier"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Supplier', txt);
+			}
+		}
 	]
 };
