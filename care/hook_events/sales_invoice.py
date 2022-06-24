5b427702b8e255195a7056a2c83ac973c99b102d
smehata <bhagwatmehta779@gmail.com>
2022-06-24 12:28:19 +0530

    validate remove service under service contract

--- care/hook_events/sales_invoice.py
+++ care/hook_events/sales_invoice.py
@@ -83,6 +83,3 @@ def create_franchise_invoice(inv, method):
             else:
                 frappe.log_error(title="Franchise Invoice Creation Error", message=response.json())
                 frappe.msgprint("Error Log Generated", indicator='red', alert=True)
-
-
-
